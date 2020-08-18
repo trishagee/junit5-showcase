2a40f1715edecd50c638f6d6c1d0924c75002698
Trisha Gee <trisha.gee@gmail.com>
2020-08-18 16:08:35 +0200

    Step 2: Simplest test.

---
+++ src/test/java/com/mechanitis/demo/junit5/ExampleTest.java
@@ -0,0 +1,11 @@
+package com.mechanitis.demo.junit5;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ExampleTest {
+    @Test
+    void shouldShowSimpleAssertion() {
+        Assertions.assertEquals(1, 1);
+    }
+}
