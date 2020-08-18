b70dacfa5cd0d24388656e6504be4a98991994e5
Trisha Gee <trisha.gee@gmail.com>
2020-08-18 16:44:42 +0200

    Step 4: Assumptions.

--- src/test/java/com/mechanitis/demo/junit5/ExampleTest.java
+++ src/test/java/com/mechanitis/demo/junit5/ExampleTest.java
@@ -1,12 +1,14 @@
 package com.mechanitis.demo.junit5;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class ExampleTest {
     @Test
@@ -23,5 +25,13 @@ class ExampleTest {
                              () -> assertEquals(3, numbers.get(1)),
                              () -> assertEquals(5, numbers.get(2)),
                              () -> assertEquals(7, numbers.get(3)));
+    }
+
+    @Test
+    @DisplayName("Should only run the test if some criteria are met")
+    void shouldOnlyRunTheTestIfSomeCriteriaAreMet() {
+        Assumptions.assumeTrue(Fixture.apiVersion() < 10);
+        // these tests only apply to a recent API version
+        assertEquals(1, 1);
     }
 }
---
+++ src/test/java/com/mechanitis/demo/junit5/Fixture.java
@@ -0,0 +1,7 @@
+package com.mechanitis.demo.junit5;
+
+public class Fixture {
+    public static int apiVersion() {
+        return 13;
+    }
+}
